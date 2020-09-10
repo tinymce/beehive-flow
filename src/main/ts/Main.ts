c6e60d644652a89a508765878055d160a409dd46
Dylan Just <>
2020-09-10 16:37:43 +1000

    Simple arg parsing.

--- src/main/ts/Main.ts
+++ src/main/ts/Main.ts
@@ -1,19 +1,35 @@
 import * as yargs from 'yargs';
 
+interface FreezeCommand {
+  kind: 'freeze'
+}
+
+const freezeCommand = (): FreezeCommand => ({
+  kind: 'freeze'
+});
+
+type BeehiveCommand = FreezeCommand;
 
 const argParser =
   yargs
     .scriptName('beehive')
+    .demandCommand(1)
     .command("freeze", "freezes the master branch");
 
 const getRealArgs = (): string[] =>
-  process.argv.slice(1)
+  process.argv.slice(2)
 
-const go = (args: string[]): void => {
-  argParser
-    .demandCommand(1)
-    .strict()
-    .parse(args);
-};
+const parseArgs = (args: string[]): Promise<BeehiveCommand> => new Promise((resolve, reject) => {
+  const a: { _: string[]; $0: string } = argParser
+  .strict()
+  .parse(args);
 
-go(getRealArgs());
+  if (a._[0] === 'freeze') {
+    resolve(freezeCommand());
+  } else {
+    reject();
+  }
+});
+
+console.log(parseArgs(getRealArgs()));
+
