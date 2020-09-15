336181a52be7e2ee5ec226f9b867c2590f1e2976
Dylan Just <dylan.just@tiny.cloud>
2020-09-15 12:08:09 +1000

    Removed excess whitespace.

--- src/main/ts/Dispatch.ts
+++ src/main/ts/Dispatch.ts
@@ -1,6 +1,5 @@
 import * as Args from './Args';
 import * as Freeze from './Freeze';
 
-
 export const dispatch = (args: Args.BeehiveCommand): Promise<void> =>
   Args.fold<Promise<void>>(args, Freeze.freeze);
