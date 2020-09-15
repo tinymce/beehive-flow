7a61a7654a14a8d39567a29ccb277bbf6fc29a74
Dylan Just <dylan.just@tiny.cloud>
2020-09-15 13:36:22 +1000

    Comment

--- src/main/ts/core/Args.ts
+++ src/main/ts/core/Args.ts
@@ -26,8 +26,11 @@ const argParser =
   .demandCommand(1)
   .command("freeze", "'freezes' the current master branch, in preparation for merging develop to master. Master will be branched as releases/x.y, some settings tweaked and pushed.");
 
+/**
+Removes the first two args, which are "node" and the script filename
+ */
 export const getRealArgs = (): string[] =>
-  process.argv.slice(2)
+  process.argv.slice(2);
 
 export const parseArgs = (args: string[]): Promise<BeehiveCommand> => new Promise((resolve, reject) => {
   const a: { _: string[]; $0: string } = argParser
