426973ef9dedf3887687595e1035b3ef0233e263
Dylan Just <dylan.just@tiny.cloud>
2020-09-15 18:36:28 +1000

    Added dry-run option

--- src/main/ts/core/Args.ts
+++ src/main/ts/core/Args.ts
@@ -1,30 +1,51 @@
 import * as yargs from 'yargs';
 import { impossible } from './Impossible';
 
-export interface FreezeCommand {
+interface BaseCommand {
+  readonly dryRun: boolean;
+}
+
+export interface FreezeCommand extends BaseCommand {
   readonly kind: 'freeze'
 }
 
-export const freezeCommand = (): FreezeCommand => ({
-  kind: 'freeze'
+export const freezeCommand = (dryRun: boolean): FreezeCommand => ({
+  kind: 'freeze',
+  dryRun
 });
 
 export type BeehiveCommand = FreezeCommand;
 
-export const fold = <T> (bh: BeehiveCommand, ifFreeze: () => T): T => {
+export const fold = <T> (bh: BeehiveCommand, ifFreeze: (dryRun: boolean) => T): T => {
   switch (bh.kind) {
     case 'freeze':
-      return ifFreeze();
+      return ifFreeze(bh.dryRun);
     default:
       return impossible(bh.kind);
   }
-}
+};
+
+export const fold_ = <T> (bh: BeehiveCommand, ifFreeze: (f: FreezeCommand) => T): T => {
+  switch (bh.kind) {
+    case 'freeze':
+      return ifFreeze(bh);
+    default:
+      return impossible(bh.kind);
+  }
+};
 
 const argParser =
   yargs
-  .scriptName('beehive')
-  .demandCommand(1)
-  .command("freeze", "'freezes' the current master branch, in preparation for merging develop to master. Master will be branched as releases/x.y, some settings tweaked and pushed.");
+    .scriptName('beehive')
+    .option('dry-run', {
+      type: 'boolean',
+      default: false,
+      description: 'Don\'t push to upstream git repo.'
+    })
+    .command(
+      "freeze",
+      "'freezes' the current master branch, in preparation for merging develop to master. Master will be branched as releases/x.y, some settings tweaked and pushed."
+    );
 
 /**
 Removes the first two args, which are "node" and the script filename
@@ -33,12 +54,12 @@ export const getRealArgs = (): string[] =>
   process.argv.slice(2);
 
 export const parseArgs = (args: string[]): Promise<BeehiveCommand> => new Promise((resolve, reject) => {
-  const a: { _: string[]; $0: string } = argParser
-  .strict()
-  .parse(args);
+  const a = argParser
+    .strict()
+    .parse(args);
 
   if (a._[0] === 'freeze') {
-    resolve(freezeCommand());
+    resolve(freezeCommand(a['dry-run']));
   } else {
     reject();
   }
