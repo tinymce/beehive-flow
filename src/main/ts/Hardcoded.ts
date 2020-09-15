cb49644a4f032fb74503ca2b36d284cc1ab09644
Dylan Just <dylan.just@tiny.cloud>
2020-09-15 12:32:56 +1000

    Remove commented-out code.

--- src/main/ts/Hardcoded.ts
+++ src/main/ts/Hardcoded.ts
@@ -2,4 +2,3 @@
 
 // TODO: not hardcode this.
 export const tinymceGitUrl: string = 'git@github.com:tinymce/tinymce.git';
-// export const tinymceGitUrl: string = 'git@github.com:tinydylan/tinymce.git';
