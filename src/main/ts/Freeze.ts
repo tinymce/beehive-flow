a4064c179f9a3960188c062ec4b69368ab1fa52d
Dylan Just <dylan.just@tiny.cloud>
2020-09-15 12:23:12 +1000

    ... and template strings for all.

--- src/main/ts/Freeze.ts
+++ src/main/ts/Freeze.ts
@@ -12,46 +12,46 @@ export const freeze = (): Promise<void> =>
   runFreeze(Hardcoded.tinymceGitUrl)
 
 const logb = (message: string): void =>
-  console.log(' - ' + message);
+  console.log(` - ${message}`);
 
 export const runFreeze = async (gitUrl: string): Promise<void> => {
   console.log("Freeze");
 
-  logb('Cloning ' + gitUrl + ' to temp folder');
+  logb(`Cloning ${gitUrl} to temp folder`);
   const { dir, git } = await Git.cloneInTempFolder(gitUrl);
-  logb('Cloned to ' + dir)
+  logb(`Cloned to ${dir}`)
 
   logb('Checking out master');
   await git.checkout('master');
 
   const pjFileName = path.resolve(dir, 'modules', 'tinymce', 'package.json');
 
-  logb('Parsing package.json file: ' + pjFileName);
+  logb(`Parsing package.json file: ${pjFileName}`);
   const pj = await PackageJson.parsePackageJsonFile(pjFileName);
-  logb('package.json has version: ' + Version.versionToString(pj.version));
+  logb(`package.json has version: ${Version.versionToString(pj.version)}`);
 
   const releaseBranchName = Version.releaseBranchName(pj.version);
 
   if (await Git.doesRemoteBranchExist(git, releaseBranchName)) {
-    throw new Error('Remote branch already exists: ' + releaseBranchName);
+    throw new Error(`Remote branch already exists: ${releaseBranchName}`);
   }
 
-  logb('Creating release branch: ' + releaseBranchName);
+  logb(`Creating release branch: ${releaseBranchName}`);
   await Git.checkoutNewBranch(git, releaseBranchName);
 
   const buildPropertiesFile = path.resolve(dir, 'build.properties');
   if (!await Files.exists(buildPropertiesFile)) {
-    logb(buildPropertiesFile + ' does not exist: creating');
+    logb(`${buildPropertiesFile} does not exist: creating`);
     await Files.writeFile(buildPropertiesFile, '');
   }
 
-  logb('Updating properties file: ' + buildPropertiesFile);
+  logb(`Updating properties file: ${buildPropertiesFile}`);
   const props = PropertiesReader(buildPropertiesFile);
   props.set('primaryBranch', releaseBranchName);
   await props.save(buildPropertiesFile);
 
   logb('Committing and pushing');
   await git.add(buildPropertiesFile);
-  await git.commit("Creating release branch: " + releaseBranchName);
+  await git.commit(`Creating release branch: ${releaseBranchName}`);
   await Git.pushNewBranch(git);
 };
