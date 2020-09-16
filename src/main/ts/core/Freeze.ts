9dec6a4381e0d5533970730e309607d806593e23
Dylan Just <dylan.just@tiny.cloud>
2020-09-17 01:08:44 +1000

    Lint fixes.

--- src/main/ts/core/Freeze.ts
+++ src/main/ts/core/Freeze.ts
@@ -16,21 +16,21 @@ export const freeze = (fc: FreezeArgs): Promise<void> =>
 export const runFreeze = async (fc: FreezeArgs, gitUrl: string): Promise<void> => {
   console.log('Freeze' + (fc.dryRun ? ' (dry-run)' : ''));
 
-console.log(`Cloning ${gitUrl} to temp folder`)
+  console.log(`Cloning ${gitUrl} to temp folder`);
   const { dir, git } = await Git.cloneInTempFolder(gitUrl);
-console.log(`Cloned to ${dir}`)
+  console.log(`Cloned to ${dir}`);
 
-console.log('Checking out master')
+  console.log('Checking out master');
   await git.checkout('master');
 
   const pjDir = path.resolve(dir, 'modules', 'tinymce');
 
-console.log(`Parsing package.json file in dir: ${pjDir}`)
+  console.log(`Parsing package.json file in dir: ${pjDir}`);
   const pj = await PackageJson.parsePackageJsonFileInFolder(pjDir);
   if (pj.version === undefined) {
     throw new Error('package.json file has no version');
   }
-console.log(`package.json has version: ${pj.version}`)
+  console.log(`package.json has version: ${pj.version}`);
 
   const version = await eitherToPromise(Version.parseVersion(pj.version));
   const releaseBranchName = Version.releaseBranchName(version);
@@ -39,28 +39,28 @@ console.log(`package.json has version: ${pj.version}`)
     throw new Error(`Remote branch already exists: ${releaseBranchName}`);
   }
 
-console.log(`Creating release branch: ${releaseBranchName}`)
+  console.log(`Creating release branch: ${releaseBranchName}`);
   await Git.checkoutNewBranch(git, releaseBranchName);
 
   const buildPropertiesFile = path.resolve(dir, 'build.properties');
   if (!await Files.exists(buildPropertiesFile)) {
-  console.log(`${buildPropertiesFile} does not exist: creating`)
+    console.log(`${buildPropertiesFile} does not exist: creating`);
     await Files.writeFile(buildPropertiesFile, '');
   }
 
-console.log(`Updating properties file: ${buildPropertiesFile}`)
+  console.log(`Updating properties file: ${buildPropertiesFile}`);
   const props = PropertiesReader(buildPropertiesFile);
   props.set('primaryBranch', releaseBranchName);
   await props.save(buildPropertiesFile);
 
-console.log('git commit')
+  console.log('git commit');
   await git.add(buildPropertiesFile);
   await git.commit(`Creating release branch: ${releaseBranchName}`);
 
   if (fc.dryRun) {
-  console.log(`dry-run - not pushing. To complete, run "git push" from ${dir}`)
+    console.log(`dry-run - not pushing. To complete, run "git push" from ${dir}`);
   } else {
-  console.log('git push')
+    console.log('git push');
     await Git.pushNewBranch(git);
   }
 };
