32c5c671c20e7e54e2247ec95a21f5d46983cf8d
Dylan Just <dylan.just@tiny.cloud>
2020-10-14 18:54:30 +1000

    Hello logs for each command.

--- src/main/ts/commands/Advance.ts
+++ src/main/ts/commands/Advance.ts
@@ -1,5 +1,10 @@
 import { AdvanceArgs } from '../args/BeehiveArgs';
+import * as Version from '../data/Version';
 
 export const advance = async (fc: AdvanceArgs): Promise<void> => {
+  const sMajorMinor = Version.majorMinorVersionToString(fc.majorMinorVersion);
+
+  const dryRunMessage = fc.dryRun ? ' (dry-run)' : '';
+  console.log(`Advance${dryRunMessage} ${sMajorMinor}`);
 
 }
