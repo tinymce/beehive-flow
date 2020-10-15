682d097ba3fcdc0d3d98feeffa3b82bd584d0de5
Dylan Just <dylan.just@tiny.cloud>
2020-10-15 18:23:27 +1000

    Test

--- src/test/ts/core/VersionTest.ts
+++ src/test/ts/core/VersionTest.ts
@@ -25,19 +25,19 @@ describe('Version', () => {
     });
 
     it('parses a correct 3-point version with preRelease', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.hexaString(1, 100), (major, minor, patch, preRelease) => {
+      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.integer(0, 100), (major, minor, patch, preRelease) => {
         const input = `${major}.${minor}.${patch}-${preRelease}`;
         const actual = Version.parseVersion(input);
-        const expected = E.right({ major, minor, patch, preRelease, buildMetaData: undefined });
+        const expected = E.right({ major, minor, patch, preRelease: String(preRelease), buildMetaData: undefined });
         assert.deepEqual(actual, expected);
       }));
     });
 
     it('parses a correct 3-point version with buildmeta', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.hexaString(1, 100), (major, minor, patch, buildMetaData) => {
+      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.integer(1, 100), (major, minor, patch, buildMetaData) => {
         const input = `${major}.${minor}.${patch}+${buildMetaData}`;
         const actual = Version.parseVersion(input);
-        const expected = E.right({ major, minor, patch, preRelease: undefined, buildMetaData });
+        const expected = E.right({ major, minor, patch, preRelease: undefined, buildMetaData: String(buildMetaData) });
         assert.deepEqual(actual, expected);
       }));
     });
