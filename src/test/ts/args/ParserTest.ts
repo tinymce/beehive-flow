dc9f5fe792ef99224ab70e488548a1cff5cb58f7
Dylan Just <dylan.just@tiny.cloud>
2020-10-26 14:23:28 +1000

    Parser test.

--- src/test/ts/args/ParserTest.ts
+++ src/test/ts/args/ParserTest.ts
@@ -19,10 +19,19 @@ describe('Parser', () => {
     });
     it('succeeds for release command', async () => {
       await fc.assert(fc.asyncProperty(fc.nat(100), fc.nat(100), async (major, minor) => {
-        console.log(`${major}.${minor}`);
         await assert.becomes(Parser.parseArgs([ 'release', `${major}.${minor}` ]), BeehiveArgs.releaseArgs(false, { major, minor }));
         await assert.becomes(Parser.parseArgs([ 'release', `${major}.${minor}`, '--dry-run' ]), BeehiveArgs.releaseArgs(true, { major, minor }));
       }));
+    });
+    it('succeeds for advance command', async () => {
+      await fc.assert(fc.asyncProperty(fc.nat(100), fc.nat(100), async (major, minor) => {
+        await assert.becomes(Parser.parseArgs([ 'advance', `${major}.${minor}` ]), BeehiveArgs.advanceArgs(false, { major, minor }));
+        await assert.becomes(Parser.parseArgs([ 'advance', `${major}.${minor}`, '--dry-run' ]), BeehiveArgs.advanceArgs(true, { major, minor }));
+      }));
+    });
+    it('succeeds for advance command', async () => {
+      await assert.becomes(Parser.parseArgs([ 'stamp' ]), BeehiveArgs.stampArgs(false));
+      await assert.becomes(Parser.parseArgs([ 'stamp', '--dry-run' ]), BeehiveArgs.stampArgs(true));
     });
   });
 });
