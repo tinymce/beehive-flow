30e7ff2450d2a06bc5f1315f5840d73991f96230
Dylan Just <dylan.just@tiny.cloud>
2020-10-27 23:04:47 +1000

    Refactor using RepoState

--- src/main/ts/commands/Advance.ts
+++ src/main/ts/commands/Advance.ts
@@ -6,11 +6,9 @@ import * as Git from '../utils/Git';
 import * as HardCoded from '../args/HardCoded';
 import * as BranchLogic from '../core/BranchLogic';
 import * as Inspect from '../core/Inspect';
-import {
-  readPackageJsonFileInDirAndRequireVersion
-} from '../core/Noisy';
 import * as RepoState from '../core/RepoState';
 import { PackageJson, writePackageJsonFileWithNewVersion } from '../core/PackageJson';
+import * as PromiseUtils from '../utils/PromiseUtils';
 
 type Version = Version.Version;
 const { versionToString } = Version;
@@ -41,10 +39,13 @@ export const advance = async (fc: AdvanceArgs): Promise<void> => {
 
   const rbn = BranchLogic.releaseBranchName(fc.majorMinorVersion);
   await Git.checkout(git, rbn);
-  const { pjFile, pj, version } = await readPackageJsonFileInDirAndRequireVersion(dir);
 
-  await BranchLogic.checkReleaseBranchReleaseVersion(version, fc.majorMinorVersion, rbn, 'package.json');
-  await go(version, pj, pjFile, git, fc, dir);
+  const r = await RepoState.detectRepoState(dir);
+  if (r.kind !== 'Release') {
+    return PromiseUtils.fail('Branch is not in Release state - can\t advance.');
+  } else {
+    await go(r.version, r.packageJson, r.packageJsonFile, git, fc, dir);
+  }
 };
 
 export const advanceCi = async (fc: AdvanceCiArgs): Promise<void> => {
@@ -54,9 +55,7 @@ export const advanceCi = async (fc: AdvanceCiArgs): Promise<void> => {
   if (r.kind !== 'Release') {
     console.log('Not in Release state - not advancing version.');
   } else {
-    console.log('Advancing to next rc version');
     const git = gitP(dir);
-    const version = r.version;
-    await go(version, r.packageJson, r.packageJsonFile, git, fc, dir);
+    await go(r.version, r.packageJson, r.packageJsonFile, git, fc, dir);
   }
 };
