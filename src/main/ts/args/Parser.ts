5620f026a82f06898c3729a822c32a599f241b35
Dylan Just <dylan.just@tiny.cloud>
2020-10-28 11:15:51 +1000

    Removed TODO - we don't really need that.

--- src/main/ts/args/Parser.ts
+++ src/main/ts/args/Parser.ts
@@ -136,5 +136,3 @@ export const parseArgs = (args: string[]): Promise<BeehiveArgs> => new Promise((
 
 export const parseProcessArgs = (): Promise<BeehiveArgs> =>
   parseArgs(getRealArgs());
-
-// TODO: add a parameter that tells beehive to keep temp checkouts.
