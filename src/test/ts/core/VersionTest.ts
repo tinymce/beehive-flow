7570c3f09e3d86bed1e9670ab3caccb8a0bcc293
Dylan Just <dylan.just@tiny.cloud>
2020-10-29 17:55:42 +1000

    Minor refactor in VersionTest.ts

--- src/test/ts/core/VersionTest.ts
+++ src/test/ts/core/VersionTest.ts
@@ -7,10 +7,14 @@ import * as EitherUtils from '../../../main/ts/utils/EitherUtils';
 
 const mmThrowMessage = 'Could not parse major.minor version string';
 
+const smallNat = () => fc.nat(200);
+
+const smallNatString = () => smallNat().map(String);
+
 describe('Version', () => {
   describe('parseVersion', () => {
     it('parses a correct 3-point version', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), (major, minor, patch) => {
+      fc.assert(fc.property(smallNat(), smallNat(), smallNat(), (major, minor, patch) => {
         const input = `${major}.${minor}.${patch}`;
         const actual = Version.parseVersion(input);
         const expected = E.right({ major, minor, patch, preRelease: undefined, buildMetaData: undefined });
@@ -19,7 +23,7 @@ describe('Version', () => {
     });
 
     it('fails on 2-point versions', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), (major, minor) => {
+      fc.assert(fc.property(smallNat(), smallNat(), (major, minor) => {
         const input = `${major}.${minor}`;
         const actual = Version.parseVersion(input);
         const expected = E.left('Could not parse version string');
@@ -28,7 +32,7 @@ describe('Version', () => {
     });
 
     it('parses a correct 3-point version with preRelease', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.integer(0, 100), (major, minor, patch, preRelease) => {
+      fc.assert(fc.property(smallNat(), smallNat(), smallNat(), smallNat(), (major, minor, patch, preRelease) => {
         const input = `${major}.${minor}.${patch}-${preRelease}`;
         const actual = Version.parseVersion(input);
         const expected = E.right({ major, minor, patch, preRelease: String(preRelease), buildMetaData: undefined });
@@ -37,7 +41,7 @@ describe('Version', () => {
     });
 
     it('parses a correct 3-point version with buildmeta', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.integer(1, 100), (major, minor, patch, buildMetaData) => {
+      fc.assert(fc.property(smallNat(), smallNat(), smallNat(), fc.integer(1, 100), (major, minor, patch, buildMetaData) => {
         const input = `${major}.${minor}.${patch}+${buildMetaData}`;
         const actual = Version.parseVersion(input);
         const expected = E.right({ major, minor, patch, preRelease: undefined, buildMetaData: String(buildMetaData) });
@@ -48,7 +52,7 @@ describe('Version', () => {
 
   describe('isReleaseVersion', () => {
     it('returns true for normal 3-point versions', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), (major, minor, patch) => {
+      fc.assert(fc.property(smallNat(), smallNat(), smallNat(), (major, minor, patch) => {
         assert.isTrue(Version.isReleaseVersion({ major, minor, patch }));
         assert.isTrue(Version.isReleaseVersion({ major, minor, patch, buildMetaData: undefined }));
         assert.isTrue(Version.isReleaseVersion({ major, minor, patch, preRelease: undefined }));
@@ -57,7 +61,7 @@ describe('Version', () => {
     });
 
     it('returns false if buildmetadata is specified', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.integer(0, 100).map(String),
+      fc.assert(fc.property(smallNat(), smallNat(), smallNat(), smallNatString(),
         (major, minor, patch, buildMetaData) => {
           assert.isFalse(Version.isReleaseVersion({ major, minor, patch, buildMetaData }));
           assert.isFalse(Version.isReleaseVersion({ major, minor, patch, buildMetaData, preRelease: undefined }));
@@ -65,7 +69,7 @@ describe('Version', () => {
     });
 
     it('returns false if prerelease is specified', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.integer(0, 100).map(String),
+      fc.assert(fc.property(smallNat(), smallNat(), smallNat(), smallNatString(),
         (major, minor, patch, preRelease) => {
           assert.isFalse(Version.isReleaseVersion({ major, minor, patch, preRelease }));
           assert.isFalse(Version.isReleaseVersion({ major, minor, patch, preRelease, buildMetaData: undefined }));
@@ -74,7 +78,7 @@ describe('Version', () => {
 
     it('returns false if prerelease and buildmetadata are specified', () => {
       fc.assert(fc.property(
-        fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.integer(0, 100).map(String), fc.integer(0, 100).map(String),
+        smallNat(), smallNat(), smallNat(), smallNatString(), smallNatString(),
         (major, minor, patch, preRelease, buildMetaData) => {
           assert.isFalse(Version.isReleaseVersion({ major, minor, patch, preRelease, buildMetaData }));
         }));
@@ -92,7 +96,7 @@ describe('Version', () => {
 
   describe('parseMajorMinorVersion', () => {
     it('parses 2-point version', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), (major, minor) => {
+      fc.assert(fc.property(smallNat(), smallNat(), (major, minor) => {
         assert.deepEqual(Version.parseMajorMinorVersion(`${major}.${minor}`), E.right({ major, minor }));
       }));
     });
@@ -106,7 +110,7 @@ describe('Version', () => {
 
   describe('parseMajorMinorVersionOrThrow', () => {
     it('parses 2-point version', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), (major, minor) => {
+      fc.assert(fc.property(smallNat(), smallNat(), (major, minor) => {
         assert.deepEqual(Version.parseMajorMinorVersionOrThrow(`${major}.${minor}`), { major, minor });
       }));
     });
@@ -120,7 +124,7 @@ describe('Version', () => {
 
   describe('majorMinorVersionToString', () => {
     it('round-trips', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), (major, minor) => {
+      fc.assert(fc.property(smallNat(), smallNat(), (major, minor) => {
         const s = `${major}.${minor}`;
         assert.deepEqual(Version.majorMinorVersionToString(Version.parseMajorMinorVersionOrThrow(s)), s);
       }));
@@ -129,7 +133,7 @@ describe('Version', () => {
 
   describe('versionToString', () => {
     it('round-trips for 3-point version', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), (major, minor, patch) => {
+      fc.assert(fc.property(smallNat(), smallNat(), smallNat(), (major, minor, patch) => {
         const sVersion = `${major}.${minor}.${patch}`;
         const version = EitherUtils.getOrThrow(Version.parseVersion(sVersion));
         const actual = Version.versionToString(version);
@@ -138,7 +142,7 @@ describe('Version', () => {
     });
 
     it('round-trips for version with prerelease', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.integer(0, 100).map(String),
+      fc.assert(fc.property(smallNat(), smallNat(), smallNat(), smallNatString(),
         (major, minor, patch, preRelease) => {
           const input = `${major}.${minor}.${patch}-${preRelease}`;
           const version = EitherUtils.getOrThrow(Version.parseVersion(input));
@@ -149,7 +153,7 @@ describe('Version', () => {
     });
 
     it('round-trips for version with buildmeta', () => {
-      fc.assert(fc.property(fc.integer(0, 200), fc.integer(0, 200), fc.integer(0, 100), fc.integer(0, 100).map(String),
+      fc.assert(fc.property(smallNat(), smallNat(), smallNat(), smallNatString(),
         (major, minor, patch, buildMetaData) => {
           const input = `${major}.${minor}.${patch}+${buildMetaData}`;
           const version = EitherUtils.getOrThrow(Version.parseVersion(input));
