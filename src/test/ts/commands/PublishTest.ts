0b9d3762900c6f88a5c4855a5a5b5583caa0dbae
Dylan Just <dylan.just@tiny.cloud>
2020-11-03 12:25:19 +1000

    Publish test.

--- src/test/ts/commands/PublishTest.ts
+++ src/test/ts/commands/PublishTest.ts
@@ -1,25 +1,30 @@
 import * as cp from 'child_process';
 import * as path from 'path';
 import { describe, it } from 'mocha';
+import { assert } from 'chai';
 import * as getPort from 'get-port';
 import * as Files from '../../../main/ts/utils/Files';
 import * as Git from '../../../main/ts/utils/Git';
 import * as Parser from '../../../main/ts/args/Parser';
 import * as Dispatch from '../../../main/ts/args/Dispatch';
 
-// import * as chai from 'chai';
-// import * as chaiAsPromised from 'chai-as-promised';
-// const assert = chai.use(chaiAsPromised).assert;
-//
-// const delay = (n: number): Promise<void> => new Promise((resolve) => {
-//   setTimeout(() => {
-//     resolve();
-//   }, n);
-// });
+type TempGit = Git.TempGit;
 
 const beehiveFlow = async (args: string[]): Promise<void> => {
   const a = await Parser.parseArgs(args);
   await Dispatch.dispatch(a);
+};
+
+const getTags = function (working: TempGit): Record<string, string> {
+  const output = cp.execSync('npm dist-tag ls @beehive-test/beehive-test', { cwd: working.dir }).toString();
+  const lines = output.split('\n').filter((x) => x.length > 0);
+  const r: Record<string, string> = {};
+
+  for (const line of lines) {
+    const [ tag, version ] = line.split(': ');
+    r[tag] = version;
+  }
+  return r;
 };
 
 describe('blah', () => {
@@ -42,37 +47,48 @@ web:
     const port = await getPort();
     const verdaccio = cp.spawn('yarn', [ 'verdaccio', '--listen', String(port), '--config', configFile ], { stdio: 'inherit' });
 
+    const publish = async (dir: string): Promise<void> => {
+      await beehiveFlow([ 'publish', '--working-dir', dir ]);
+    };
+
     try {
       const working = await Git.initInTempFolder(false);
-      await Git.checkoutNewBranch(working.git, 'feature/TINY-BLAH');
+      const branchName = 'feature/TINY-BLAH';
+      await Git.checkoutNewBranch(working.git, branchName);
 
       const npmrc = `@beehive-test:registry=http://0.0.0.0:${port}/`;
       const npmrcFile = path.join(working.dir, '.npmrc');
       await Files.writeFile(npmrcFile, npmrc);
 
-      const pj = `
-      {
-        "name": "@beehive-test/beehive-test",
-        "version": "1.2.3",
-        "publishConfig": {
-          "@beehive-test:registry": "http://0.0.0.0:${port}/"
-        }
-      }`;
-
       const pjFile = path.join(working.dir, 'package.json');
-      await Files.writeFile(pjFile, pj);
 
-      await working.git.add([ npmrcFile, pjFile ]);
-      await working.git.commit('feature branch');
+      const go = async (version: string) => {
+        const pj = `
+        {
+          "name": "@beehive-test/beehive-test",
+          "version": "${version}",
+          "publishConfig": {
+            "@beehive-test:registry": "http://0.0.0.0:${port}/"
+          }
+        }`;
+        await Files.writeFile(pjFile, pj);
+        await working.git.add([ npmrcFile, pjFile ]);
+        await working.git.commit('commit');
+        await publish(working.dir);
+        return getTags(working);
+      };
 
-      await beehiveFlow([ 'publish', '--working-dir', working.dir ]);
+      const version1 = `0.1.0-alpha`;
+      const tags1 = await go(version1);
 
-      // TODO: validate that the tag worked
-      // TODO: test with a release version and check the second tag
+      // NOTE: first publish always ends up tagged latest
+      assert.deepEqual(tags1, { latest: version1, [ branchName ]: version1 });
 
-      // TODO: It seems to be ignoring the tag and just using "latest"
-      cp.execSync('npm dist-tag ls @beehive-test/beehive-test', { stdio: 'inherit', cwd: working.dir });
+      const version2 = `0.2.0-alpha`;
+      const tags2 = await go(version2);
 
+      // NOTE: first publish always ends up tagged latest
+      assert.deepEqual(tags2, { latest: version1, [ branchName ]: version2 });
     } finally {
       verdaccio.kill();
     }
