8e301f527ade83934af32c53b67aa4a4ea22c514
techtangents <167879+techtangents@users.noreply.github.com>
2020-12-01 09:44:27 +1000

    Refactor

--- src/main/ts/core/NpmTags.ts
+++ src/main/ts/core/NpmTags.ts
@@ -2,22 +2,22 @@ import { SimpleGit } from 'simple-git/promise';
 import * as Git from '../utils/Git';
 import * as PromiseUtils from '../utils/PromiseUtils';
 import * as ArrayUtils from '../utils/ArrayUtils';
-import { BranchState, versionFromReleaseBranch, getReleaseBranchName } from './BranchLogic';
+import { BranchState, getReleaseBranchName, versionFromReleaseBranch } from './BranchLogic';
 import * as Version from './Version';
 
 export const pickTags = async (branchName: string, branchState: BranchState, getBranches: () => Promise<string[]>): Promise<[string, string?]> => {
   const mainTag = branchName;
 
-  const tagsForReleaseReadyState = async (): Promise<[string, string?]> => {
-    const branches = await getBranches();
-    const versions = await PromiseUtils.filterMap(branches, versionFromReleaseBranch);
-    const greatestOpt = ArrayUtils.greatest(versions, Version.compareMajorMinorVersions);
-    const greatest = await PromiseUtils.optionToPromise(greatestOpt, 'Could not find any release branches with valid names.');
-    const releaseBranchName = getReleaseBranchName(greatest);
-    return branchName === releaseBranchName ? [ mainTag, 'latest' ] : [ mainTag ];
-  };
+  if (branchState !== BranchState.ReleaseReady) {
+    return [ mainTag ];
+  }
 
-  return branchState === BranchState.ReleaseReady ? await tagsForReleaseReadyState() : [ mainTag ];
+  const branches = await getBranches();
+  const versions = await PromiseUtils.filterMap(branches, versionFromReleaseBranch);
+  const greatestOpt = ArrayUtils.greatest(versions, Version.compareMajorMinorVersions);
+  const greatest = await PromiseUtils.optionToPromise(greatestOpt, 'Could not find any release branches with valid names.');
+  const releaseBranchName = getReleaseBranchName(greatest);
+  return branchName === releaseBranchName ? [ mainTag, 'latest' ] : [ mainTag ];
 };
 
 export const pickTagsGit = async (branchName: string, branchState: BranchState, git: SimpleGit): Promise<[string, string?]> =>
