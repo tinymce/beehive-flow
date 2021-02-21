b02394954579a08f40e70e7061f1fd6cbea12708
Dylan Just <167879+techtangents@users.noreply.github.com>
2021-02-21 19:03:57 +1000

    ObjUtilsTest

---
+++ src/test/ts/utils/ObjUtilsTest.ts
@@ -0,0 +1,26 @@
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+import * as ObjUtils from '../../../main/ts/utils/ObjUtils';
+
+import fc from 'fast-check';
+
+describe('ObjUtils', () => {
+  describe('fromPairs', () => {
+    it('returns empty object for empty array', () => {
+      assert.deepEqual(ObjUtils.fromPairs([]), {});
+    });
+
+    it('returns single-key object for single-element array', () => {
+      fc.assert(fc.property(fc.string(), fc.nat(), (k, v) => {
+        assert.deepEqual(ObjUtils.fromPairs([ [ k, v ] ]), { [k]: v });
+      }));
+    });
+
+    it('returns correct objects for larger arrays', () => {
+      assert.deepEqual(
+        ObjUtils.fromPairs([ [ 'a', 'zingo' ], [ 'hasOwnProperty', '*' ] ]),
+        { 'a': 'zingo', 'hasOwnProperty': '*' }
+      );
+    });
+  });
+});
