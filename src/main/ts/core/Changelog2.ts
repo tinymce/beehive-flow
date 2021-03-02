1d09ca1e1b1336495dd4076f6ac6d73425aec9cd
techtangents <167879+techtangents@users.noreply.github.com>
2021-03-02 22:23:56 +1000

    Progress

--- src/main/ts/core/Changelog2.ts
+++ src/main/ts/core/Changelog2.ts
@@ -10,22 +10,12 @@ type Parser<I, A> = P.Parser<I, A>;
 type Token = marked.Token;
 type Def = marked.Tokens.Def;
 
-// export const children = (node: Node): Node[] => {
-//   const items = [];
-//   let item = node.firstChild;
-//   while (item) {
-//     items.push(item);
-//     item = item.next;
-//   }
-//   return items;
-// };
-//
-// export const childrenStream = (node: Token): Stream<Node> =>
-//   S.stream(children(node));
-
 // Def doesn't have a 'type' field for some reason
 export const isNotDef = (token: Token): token is Exclude<Token, Def> =>
   Object.prototype.hasOwnProperty.call(token, 'type');
+
+export const isDef = (token: Token): token is Def =>
+  !Object.prototype.hasOwnProperty.call(token, 'type');
 
 export const isType = (token: Token, type: string): boolean =>
   isNotDef(token) && token.type === type;
@@ -33,42 +23,66 @@ export const isType = (token: Token, type: string): boolean =>
 export const isHeading = (token: Token): token is Heading =>
   isType(token, 'heading');
 
-export const parseHeadingLevel = (level: number): Parser<Token, Heading> =>
+const parseHeading: Parser<Token, Heading> =
   P.expected(
     pipe(
       P.item<Token>(),
-      P.filter(isHeading),
+      P.filter(isHeading)
+    ),
+    `Expected heading`
+  );
+
+const parseHeadingLevel = (level: number): Parser<Token, Heading> =>
+  P.expected(
+    pipe(
+      parseHeading,
       P.filter((t) => t.depth === level)
     ),
     `Expected heading ${level}`
   );
 
-export const parseChangelogHeader = (): Parser<Token, Heading> =>
+const headingLiteral = (level: number, text: string): Parser<Token, Heading> =>
   P.expected(
     pipe(
-      parseHeadingLevel(1),
-      P.filter((token) => token.text === 'Changelog')
+      parseHeadingLevel(level),
+      P.filter((token) => token.text === text)
     ),
-    'Changelog must start with: "# Changelog"'
+    `Expected heading ${level} with exact text: "${text}"`
   );
 
-type Changelog = string | null;
+const textSection: Parser<Token, Token[]> =
+  P.expected(
+    P.many1(P.sat((t) => isNotDef(t) && ['paragraph', 'space', 'text'].includes(t.type))),
+    'Expected section of text'
+  );
+
+const parseChangelogHeader = headingLiteral(1, 'Changelog');
+const parseUnreleasedHeading = headingLiteral(2, 'Unreleased');
+
+type Changelog = null;
 
 export const parseChangelog = (): Parser<Token, Changelog> =>
   pipe(
-    parseChangelogHeader(),
-    P.map((token) => token.text)
+    parseChangelogHeader,
+    P.apSecond(textSection),
+    P.apSecond(parseUnreleasedHeading),
+    P.apSecond(P.succeed(null))
   );
 
 export const doParse = (input: string) => {
   // const markdownParser = new commonmark.Parser();
   // const markdown = markdownParser.parse(input);
-  // TODO: not null
+
 
   const options: marked.MarkedOptions = { gfm: true };
+
+  // TODO: check if this fails
   const tokens = marked.lexer(input, options);
 
+  const result = parseChangelog()(S.stream(tokens));
 
+  // TODO: remove
+  console.log(result);
 
-  return parseChangelog()(S.stream(tokens));
+  return result;
 };
--- src/test/ts/core/Changelog2Test.ts
+++ src/test/ts/core/Changelog2Test.ts
@@ -3,17 +3,38 @@ import { assert } from 'chai';
 import * as E from 'fp-ts/Either';
 
 import * as Changelog from '../../../main/ts/core/Changelog2';
+import { pipe } from 'fp-ts/function';
 
 describe('changelog', () => {
-  it('fails if starts with heading 2', () => {
-    assert.isTrue(E.isLeft(Changelog.doParse(`## Changelog`)));
-  });
 
-  it('passes if starts with heading 1', () => {
-    assert.isTrue(E.isRight(Changelog.doParse(`# Changelog`)));
+  const go = (input: string) =>
+    pipe(
+      Changelog.doParse(input),
+      E.bimap(
+        (error) => error.expected.join(','),
+        (ok) => ok.value
+      )
+    );
+
+  it('fails if starts with heading 2', () => {
+    assert.deepEqual(go('## Changelog'), E.left('Expected heading 1 with exact text: "Changelog"'));
   });
 
   it('fails if starts with heading 1 with wrong text', () => {
     assert.isTrue(E.isLeft(Changelog.doParse(`# Changeblog`)));
   });
+
+  it('fails if starts with heading 1 but is missing blurb', () => {
+    assert.deepEqual(go(`# Changelog`), E.left('Expected section of text'));
+  });
+
+  it('parses changelog header text', () => {
+    assert.isTrue(E.isLeft(Changelog.doParse(`# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+`)));
+  });
+
 });
