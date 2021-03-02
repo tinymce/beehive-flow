2722ea9beba08a25d53aaf484b5e753f09d8ef8f
techtangents <167879+techtangents@users.noreply.github.com>
2021-03-02 22:27:26 +1000

    Test

--- src/test/ts/core/Changelog2Test.ts
+++ src/test/ts/core/Changelog2Test.ts
@@ -28,13 +28,15 @@ describe('changelog', () => {
     assert.deepEqual(go(`# Changelog`), E.left('Expected section of text'));
   });
 
-  it('parses changelog header text', () => {
-    assert.isTrue(E.isLeft(Changelog.doParse(`# Changelog
+  it('parses changelog header text with Unreleased header (minimum complete example', () => {
+    assert.deepEqual(go(`# Changelog
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
-`)));
-  });
 
+## Unreleased
+`
+    ), E.right(null));
+  });
 });
