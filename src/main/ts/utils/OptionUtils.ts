177253b22695432b3683f53fd54b308ae3801403
Lee Newson <lee.newson@tiny.cloud>
2021-05-20 17:26:00 +1000

    Minor cleanup to not use internal fp-ts `_tag` variables

--- src/main/ts/utils/OptionUtils.ts
+++ src/main/ts/utils/OptionUtils.ts
@@ -3,10 +3,10 @@ import * as O from 'fp-ts/Option';
 type Option<A> = O.Option<A>;
 
 export const eachAsync = async <A> (o: Option<A>, f: (a: A) => Promise<void>): Promise<void> => {
-  if (o._tag === 'Some') {
+  if (O.isSome(o)) {
     return f(o.value);
   }
 };
 
 export const mapAsync = async <A, B> (o: Option<A>, f: (a: A) => Promise<B>): Promise<Option<B>> =>
-  o._tag === 'Some' ? O.some(await f(o.value)) : O.none;
+  O.isSome(o) ? O.some(await f(o.value)) : O.none;
