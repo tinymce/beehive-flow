37aac04a6e34df35143eb97139bddb458bc31d84
Lee Newson <lee.newson@tiny.cloud>
2021-07-11 08:04:00 +1000

    TINY-7701: Changed spike branches to use the alpha version prefix

--- src/test/ts/commands/ReviveTest.ts
+++ src/test/ts/commands/ReviveTest.ts
@@ -24,30 +24,32 @@ describe('Revive', () => {
     return { git, dir };
   };
 
+  const TIMEOUT = 4000;
+
   it('revives version from 1.0.0 tag', async () => {
     const { git, dir } = await runScenario([ '0.1.0', '1.0.0' ], [], '1.0');
     await assert.becomes(Git.currentBranch(git), 'release/1.0');
     await assert.becomes(readPjVersionInDir(dir), '1.0.1-rc');
-  });
+  }).timeout(TIMEOUT);
 
   it('revives version when multiple tags exist', async () => {
     const { git, dir } = await runScenario([ '0.1.0', '1.0.0', '1.1.0', '1.1.1', '1.1.2' ], [ 'release/1.0' ], '1.1');
     await assert.becomes(Git.currentBranch(git), 'release/1.1');
     await assert.becomes(readPjVersionInDir(dir), '1.1.3-rc');
-  });
+  }).timeout(TIMEOUT);
 
   it('fails if a release branch already exists', async () => {
     const result = runScenario([ '1.0.0' ], [ 'release/1.0' ], '1.0');
     await assert.isRejected(result, 'Remote branch already exists: release/1.0');
-  });
+  }).timeout(TIMEOUT);
 
   it('fails if main is at the same major.minor version', async () => {
     const result = runScenario([ '0.0.1' ], [], '0.1');
     await assert.isRejected(result, 'main branch is still at version: 0.1');
-  });
+  }).timeout(TIMEOUT);
 
   it('fails if no tags exist for the specified major.major version', async () => {
     const result = runScenario([ '0.0.1' ], [], '1.0');
     await assert.isRejected(result, 'Failed to find any tags matching version: 1.0');
-  });
+  }).timeout(TIMEOUT);
 });
